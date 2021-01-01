fe8bf2e53dcf6a3850ec64a269290cb3783aa632
Ilgam Kurbanov <ikurbanov015@gmail.com>
2021-01-01 12:16:38 +0300

    deploy url

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('express')()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://stoic-wiles-9cc80d.netlify.app/',
     methods: ['GET', 'POST']
   }
 })
