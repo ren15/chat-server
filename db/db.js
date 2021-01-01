1aa4d9658c8216b531bdaf22e9e5e769a5b777d8
Ilgam Kurbanov <ikurbanov015@gmail.com>
2021-01-01 13:04:33 +0300

    delete console.log

--- db/db.js
+++ db/db.js
@@ -5,8 +5,6 @@ require('dotenv').config()
 const chatSchema = schemas.chatSchema
 const userSchema = schemas.userSchema
 
-console.log(process.env.DB_USER)
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ct7vt.mongodb.net/chat-db?retryWrites=true&w=majority`
 
 // подключение
